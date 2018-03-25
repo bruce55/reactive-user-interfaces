f0dc90fc5f96e7c45bc9915f14c65463cfb1da7f
Bruce Luo <bruce.luo+git@nyu.edu>
2018-03-26 04:11:20 +0800

    handle new contact

--- reactive-user-interfaces/src/components/ContactField.js
+++ reactive-user-interfaces/src/components/ContactField.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react';
 import './ContactField.css';
 
 class ContactField extends Component {
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            value: ''
+        }
+    }
+
+    onChange(e) {
+        this.props.onChange(this.props.label, e.target.value);
+        this.setState({
+            value: e.target.value
+        })
+    }
+
     render() {
-        if (this.props.fields) {
-            const fields = this.props.fields.map((field, i) =>
-                <li key={this.props.label + i}>{field}</li>
+        if (this.props.edit) {
+            return (
+                <div className="contact-field">
+                    <i className="material-icons">{this.props.icon}</i>
+                    <input onChange={this.onChange} value={this.state.value}></input>
+                </div>
             )
+        } else if (this.props.fields) {
+            let fields;
+            if (Array.isArray(this.props.fields)) {
+                fields = this.props.fields.map((field, i) =>
+                    <li key={this.props.label + i}>{field}</li>
+                )
+            } else {
+                fields = <li>{this.props.fields}</li>
+            }
+            
 
             return (
                 <div className="contact-field">
@@ -14,15 +42,9 @@ class ContactField extends Component {
                     <ul>{fields}</ul>
                 </div>
             );
-        } else if (this.props.edit) {
-            return (
-                <div className="contact-field">
-                    <i className="material-icons">{this.props.icon}</i>
-                    <input></input>
-                </div>
-            )
+        } else {
+            return ''
         }
-        return ''
     }
 }
 
--- reactive-user-interfaces/src/routes/CreateContactPage.js
+++ reactive-user-interfaces/src/routes/CreateContactPage.js
@@ -13,10 +13,24 @@ class CreateContactPage extends Component {
         this.state = {
             contact: new Contact()
         }
+
+        this.updateField = this.updateField.bind(this);
+        this.onClick = this.onClick.bind(this);
     }
 
     goBack() {
         window.history.back();
+    }
+
+    updateField(field, text) {
+        this.setState((previousState) => {
+            previousState.contact[field] = text;
+            return previousState;
+        })
+    }
+
+    onClick() {
+        this.props.addContact(this.state.contact)
     }
 
     render() {
@@ -25,14 +39,14 @@ class CreateContactPage extends Component {
                 <h1 style={{ backgroundColor: this.state.contact.color }}>
                     <Link to="#" onClick={this.goBack}><i className="material-icons">arrow_back</i></Link>
                     <span>New Contact</span>
-                    <i className="material-icons">save</i>
+                    <i className="material-icons" onClick={this.onClick}>save</i>
                 </h1>
                 <div id="page-container">
-                    <ContactField icon="person" fields={this.state.contact.name} label="name" edit/>
-                    <ContactField icon="phone" fields={this.state.contact.numbers} label="numbers" edit/>
-                    <ContactField icon="email" fields={this.state.contact.emails} label="emails" edit/>
-                    <ContactField icon="location_city" fields={this.state.contact.addrs} label="addrs" edit/>
-                    <ContactField icon="location_on" fields={this.state.contact.country} label="country" edit/>
+                    <ContactField icon="person" fields={this.state.contact.name} label="name" edit onChange={this.updateField}/>
+                    <ContactField icon="phone" fields={this.state.contact.numbers} label="numbers" edit onChange={this.updateField}/>
+                    <ContactField icon="email" fields={this.state.contact.emails} label="emails" edit onChange={this.updateField}/>
+                    <ContactField icon="location_city" fields={this.state.contact.addrs} label="addrs" edit onChange={this.updateField}/>
+                    <ContactField icon="location_on" fields={this.state.contact.country} label="country" edit onChange={this.updateField}/>
                 </div>
             </div>
         );
