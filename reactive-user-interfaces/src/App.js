0c603e0503502f9d2869c1b494e6b66ddb1361cf
Bruce Luo <bruce.luo+git@nyu.edu>
2018-03-26 14:40:31 +0800

    inclass update

--- reactive-user-interfaces/src/App.js
+++ reactive-user-interfaces/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=2663a6d57fc14ea28fb2c6aa07827f5d').then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText);
+            }
+        }).then(data => {
+            this.setState({
+                articles: data.response.docs,
+                isLoading: false
+            })
+        })
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return (
+                <div className="App">
+                    <p>Loading...</p>
+                </div>
+            )
+        } else {
+            return <div className="App">
+                <h1>{this.state.articles[0].headline.main}</h1>
+                <p>{this.state.articles[0].snippet}</p>
+                <a href={this.state.articles[0].web_url}>Open Here</a>
+            </div>
+        }
+
+    }
 }
 
 export default App;
